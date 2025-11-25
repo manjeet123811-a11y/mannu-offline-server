d6703bd1670254daa18ed2c5a1de725881b33720
manjeet123811-a11y <manjeet123811@gmail.com>
2025-11-25 21:33:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import threading, time
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import database as db
